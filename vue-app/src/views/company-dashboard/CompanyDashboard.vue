4ae72e58087d82db8745ef251ecd964e1b413c08
Maurendra <mrendra25@gmail.com>
2022-03-02 16:32:20 +0700

    Init to create a new simple app

---
+++ vue-app/src/views/company-dashboard/CompanyDashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="w-2/6 mb-4">
+        <p className="text-2xl font-bold">Vue App</p>
+        <p className="mb-4 text-xs">Event Organizer</p>
+        <div className="bg-blue-900 p-8 rounded-lg border-2 ">
+          <h1 className="font-bold text-4xl text-white mb-4">Sign In</h1>
+          <div className="mb-4">
+            <div className="mb-4">
+              <div className="w-full bg-neutral-50 py-4 px-8 border-2 border-neutral-300 items-center rounded-lg flex space-x-4">
+                <input
+                  type="text"
+                  className="text-neutral-500 placeholder-gray-300 text-base leading-6 flex-1 bg-neutral-50 border-none focus:outline-none"
+                  placeholder="Username"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="w-full bg-neutral-50 py-4 px-8 border-2 border-neutral-300 items-center rounded-lg flex space-x-4">
+                <input
+                  type="password"
+                  className="text-neutral-500 placeholder-gray-300 text-base leading-6 flex-1 bg-neutral-50 border-none focus:outline-none"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex items-center w-full justify-center"
+          >
+            <div className="bg-white hover:bg-blue-400 hover:text-white border-2 border-neutral-600 rounded-lg py-4 px-12 cursor-pointer w-fit">
+              <p className="font-bold">Sign In</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
